51348452b514f2eadaed56ac92d1517009635fbd
Peyman <118217228+Ptechgithub@users.noreply.github.com>
2024-09-04 11:45:31 +0330

    update ham_keys.sh

--- ham_keys.sh
+++ ham_keys.sh
@@ -73,6 +73,14 @@ games[7, name]="Mow and Trim"
 games[7, appToken]="ef319a80-949a-492e-8ee0-424fb5fc20a6"
 games[7, promoId]="ef319a80-949a-492e-8ee0-424fb5fc20a6"
 
+games[8, name]="Fluff Crusade"
+games[8, appToken]="112887b0-a8af-4eb2-ac63-d82df78283d9"
+games[8, promoId]="112887b0-a8af-4eb2-ac63-d82df78283d9"
+
+games[9, name]="Tile Trio"
+games[9, appToken]="e68b39d2-4880-4a31-b3aa-0393e7df10c7"
+games[9, promoId]="e68b39d2-4880-4a31-b3aa-0393e7df10c7"
+
 # Proxys
 load_proxies() {
 	if [[ -f "$1" ]]; then
@@ -204,7 +212,7 @@ main() {
 	load_proxies "$PROXY_FILE"
 
 	while true; do
-		for game_choice in {1..7}; do
+		for game_choice in {1..9}; do
 			if [[ ${#proxies[@]} -gt 0 ]]; then
 				proxy=${proxies[RANDOM % ${#proxies[@]}]}
 			else
